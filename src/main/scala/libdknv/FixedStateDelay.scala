0aeca076479114638d54a1925006651fb64f64d1
denis <dknvdenis@gmail.com>
2021-04-29 13:21:04 +0300

    Добавлен FixedStateDelay - исправленная версия Fsm::StateDelay, которая правильно работает внутри SlowArea.

---
+++ src/main/scala/libdknv/FixedStateDelay.scala
@@ -0,0 +1,27 @@
+package libdknv
+
+import spinal.core._
+import spinal.lib.fsm._
+import scala.math._, BigDecimal._
+
+class FixedStateDelay(cyclesCount: UInt)(implicit stateMachineAccessor: StateMachineAccessor) extends State with StateCompletionTrait {
+
+  /** Create a StateDelay with an TimeNumber */
+  def this(time: TimeNumber, addCycles: Int = 0)(implicit stateMachineAccessor: StateMachineAccessor){
+    this((time * ClockDomain.current.samplingRate.getValue).setScale(0, RoundingMode.CEILING).toBigInt + addCycles)
+  }
+
+  val cache = stateMachineAccessor.cacheGetOrElseUpdate(StateMachineSharableUIntKey, new StateMachineSharableRegUInt).asInstanceOf[StateMachineSharableRegUInt]
+  cache.addMinWidth(cyclesCount.getWidth)
+
+  onEntry{
+    cache.value := cyclesCount
+  }
+
+  whenIsActive{
+    cache.value := cache.value - 1
+    when(cache.value <= 1){
+      doWhenCompletedTasks()
+    }
+  }
+}
